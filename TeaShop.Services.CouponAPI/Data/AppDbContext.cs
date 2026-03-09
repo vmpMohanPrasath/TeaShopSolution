7af78770cc6660945f2617e959c7fffb40a123cb
Mohan Prasath <mpmohanprasath@gmail.com>
2026-03-09 23:29:16 +0530

    Add project files.

---
+++ TeaShop.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaShop.Services.CouponAPI.Models;
+
+namespace TeaShop.Services.CouponAPI.Data
+{
+    public class AppDbContext: DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
